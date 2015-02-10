f73883117ba464dfad277568bd9bf7d59374f166
Marcio Gasparotto <mgasparotto@gmail.com>
2015-02-09 23:06:53 -0200

    working with styles

--- src/skylight.jsx
+++ src/skylight.jsx
@@ -1,5 +1,38 @@
 var React = require('react');
 
+var overlayStyles = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    zIndex: 99,
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    display: 'block'
+};
+
+var dialogStyles = {
+    width: '50%',
+    height: '400px',
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    marginTop: '-200px',
+    marginLeft: '-25%',
+    backgroundColor: '#fff',
+    borderRadius: '2px',
+    zIndex: 100,
+    padding: '10px',
+    boxShadow: '0 0 4px rgba(0,0,0,.14),0 4px 8px rgba(0,0,0,.28)'
+};
+
+var closeButtonStyle = {
+    cursor: 'pointer',
+    float: 'right',
+    fontSize: '1.6em',
+    margin: '-15px 0'
+};
+
 var SkyLight = React.createClass({
     propTypes: {
         title: React.PropTypes.string,
@@ -7,7 +40,9 @@ var SkyLight = React.createClass({
         beforeOpen: React.PropTypes.func,
         afterOpen: React.PropTypes.func,
         beforeClose: React.PropTypes.func,
-        afterClose: React.PropTypes.func
+        afterClose: React.PropTypes.func,
+        styleWidth: React.PropTypes.string,
+        styleHeight: React.PropTypes.string
     },
     getDefaultProps: function () {
         return {
@@ -47,17 +82,24 @@ var SkyLight = React.createClass({
     render: function () {
 
         var overlay;
-        var displayStyle = this.state.isVisible ? {display: 'block'} : {display: 'none'};
+
+        if (this.state.isVisible) {
+            overlayStyles.display = 'block';
+            dialogStyles.display = 'block';
+        } else {
+            overlayStyles.display = 'none';
+            dialogStyles.display = 'none';
+        }
 
         if (this.props.showOverlay) {
-            overlay = (<div className="skylight-dialog__overlay" style={displayStyle}></div>);
+            overlay = (<div className="skylight-dialog__overlay"  style={overlayStyles}></div>);
         }
 
         return (
             <section className="skylight-wrapper">
                 {overlay}
-                <div className="skylight-dialog" style={displayStyle}>
-                    <a role="button" className="skylight-dialog--close" onClick={this.hide}>&times;</a>
+                <div className="skylight-dialog" style={dialogStyles}>
+                    <a role="button" className="skylight-dialog--close" style={closeButtonStyle} onClick={this.hide}>&times;</a>
                     <h2>{this.props.title}</h2>
                     {this.props.children}
                 </div>

84bf90d33379387b28094eeead570884b0912142
Alexandr Usachev <alex.fe.us@gmail.com>
2015-07-14 16:07:06 +0300

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -1,32 +1,6 @@
 
 module.exports = {
-    overlayStyles: {
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        zIndex: 99,
-        backgroundColor: 'rgba(0,0,0,0.3)'
-    },
-    dialogStyles: {
-        width: '50%',
-        height: '400px',
-        position: 'fixed',
-        top: '50%',
-        left: '50%',
-        marginTop: '-200px',
-        marginLeft: '-25%',
-        backgroundColor: '#fff',
-        borderRadius: '2px',
-        zIndex: 100,
-        padding: '10px',
-        boxShadow: '0 0 4px rgba(0,0,0,.14),0 4px 8px rgba(0,0,0,.28)'
-    },
-    closeButtonStyle: {
-        cursor: 'pointer',
-        float: 'right',
-        fontSize: '1.6em',
-        margin: '-15px 0'
-    }
+    overlayStyles: {},
+    dialogStyles: {},
+    closeButtonStyle: {}
 };

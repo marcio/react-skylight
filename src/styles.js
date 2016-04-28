50f7fdecd1209ec57152101a931640ef00f2bab9
Marcio Gasparotto <mgasparotto@gmail.com>
2016-04-27 21:22:13 -0300

    fix #35

--- src/styles.js
+++ src/styles.js
@@ -2,11 +2,11 @@
 const styles = {
   overlayStyles: {
     position: 'fixed',
-    top: 0,
-    left: 0,
+    top: '0px',
+    left: '0px',
     width: '100%',
     height: '100%',
-    zIndex: 99,
+    zIndex: '99',
     backgroundColor: 'rgba(0,0,0,0.3)'
   },
   dialogStyles: {
@@ -19,19 +19,19 @@ const styles = {
     marginLeft: '-25%',
     backgroundColor: '#fff',
     borderRadius: '2px',
-    zIndex: 100,
+    zIndex: '100',
     padding: '15px',
-    boxShadow: '0 0 4px rgba(0,0,0,.14),0 4px 8px rgba(0,0,0,.28)'
+    boxShadow: '0px 0px 4px rgba(0,0,0,.14),0px 4px 8px rgba(0,0,0,.28)'
   },
   title: {
-    marginTop: '0'
+    marginTop: '0px'
   },
   closeButtonStyle: {
     cursor: 'pointer',
     position: 'absolute',
     fontSize: '1.8em',
     right: '10px',
-    top: '0'
+    top: '0px'
   }
 };
 

2d269ed32eb7e71a0a3e22a0918ec594b773da7b
Marcio Gasparotto <mgasparotto@gmail.com>
2018-10-17 16:43:31 -0300

    wip

--- src/skylightstateless.jsx
+++ src/skylightstateless.jsx
@@ -4,21 +4,20 @@ import styles from './styles';
 import assign from './utils/assign';
 
 export default class SkyLightStateless extends React.Component {
-
   componentWillMount() {
-    document.addEventListener("keydown", this._handlerEsc.bind(this));
+    document.addEventListener('keydown', this._handlerEsc.bind(this));
   }
 
   componentWillUnmount() {
-    document.removeEventListener("keydown", this._handlerEsc.bind(this));
+    document.removeEventListener('keydown', this._handlerEsc.bind(this));
   }
 
   _handlerEsc(evt) {
     var isEscape = false;
-    if ("key" in evt) {
-      isEscape = (evt.key == "Escape" || evt.key == "Esc");
+    if ('key' in evt) {
+      isEscape = evt.key == 'Escape' || evt.key == 'Esc';
     } else {
-      isEscape = (evt.keyCode == 27);
+      isEscape = evt.keyCode == 27;
     }
     if (isEscape && this.props.closeOnEsc && this.props.isVisible) {
       this.props.onCloseClicked();
@@ -41,45 +40,82 @@ export default class SkyLightStateless extends React.Component {
     const mergeStyles = key => assign({}, styles[key], this.props[key]);
     const { isVisible } = this.props;
     const dialogStyles = mergeStyles('dialogStyles');
-    const overlayStyles = mergeStyles('overlayStyles');
+    // const overlayStyles = mergeStyles('overlayStyles');
     const closeButtonStyle = mergeStyles('closeButtonStyle');
     const titleStyle = mergeStyles('titleStyle');
-    
-    let finalStyle;
-    if(isVisible) {
-      finalStyle = assign({}, dialogStyles, styles.animationOpen);
-      overlayStyles.display = 'block';
-    } else {
-      finalStyle = assign({}, dialogStyles, styles.animationBase);
-      overlayStyles.display = 'none';
-    }
-    
-    finalStyle.transitionDuration = `${this.props.transitionDuration}ms`;
-    overlayStyles.transitionDuration = `${this.props.transitionDuration}ms`;
 
-    let overlay;
-    if (this.props.showOverlay) {
-      overlay = (
-        <div className="skylight-overlay"
-          onClick={() => this.onOverlayClicked()}
-          style={overlayStyles}
-        />
-      );
+    let wrapperStyles = {
+      top: '0px',
+      bottom: '0px',
+      left: '0px',
+      right: '0px',
+      position: 'fixed',
+      overflowX: 'auto',
+      overflowY: 'auto',
+      backgroundColor: 'rgba(0,0,0, .25)',
+      transitionProperty: 'all',
+      transitionTimingFunction: 'ease',
+      zIndex: 1000,
+      opacity: 0,
+    };
+
+    let modalWrapper = { width: 'auto', position: 'relative' };
+
+    let finalStyle;
+
+    if (isVisible) {
+      // finalStyle = assign({}, dialogStyles, styles.animationOpen);
+      // overlayStyles.display = 'block';
+      // wrapperStyles = assign({}, wrapperStyles, styles.animationOpen);
+      // wrapperStyles.transition = 'opacity 5s linear';
+      wrapperStyles.opacity = 1;
+      wrapperStyles.display = 'block';
+      // modalWrapper = assign({}, modalWrapper, styles.animationOpen);
+    } else {
+      // finalStyle = assign({}, dialogStyles, styles.animationBase);
+      // overlayStyles.display = 'none';
+      // wrapperStyles = assign({}, wrapperStyles, styles.animationBase);
+      // wrapperStyles.transition = 'opacity 5s linear';
+      wrapperStyles.opacity = 0;
+      wrapperStyles.display = 'none';
+      // modalWrapper = assign({}, modalWrapper, styles.animationBase);
     }
+
+    // finalStyle.transitionDuration = `${this.props.transitionDuration}ms`;
+    // overlayStyles.transitionDuration = `${this.props.transitionDuration}ms`;
+    // wrapperStyles.transitionDuration = `${this.props.transitionDuration}ms`;
+    // modalWrapper.transitionDuration = `${this.props.transitionDuration}ms`;
+
+    // let overlay;
+    // if (this.props.showOverlay) {
+    //   overlay = (
+    //     <div
+    //       className="skylight-overlay"
+    //       onClick={() => this.onOverlayClicked()}
+    //       style={overlayStyles}
+    //     />
+    //   );
+    // }
 
     let title;
-    if(React.isValidElement(this.props.title)){
+    if (React.isValidElement(this.props.title)) {
       title = this.props.title;
     } else {
-      title = this.props.title ? <h2 style={titleStyle}>{this.props.title}</h2> : null;
+      title = this.props.title ? (
+        <h2 style={titleStyle}>{this.props.title}</h2>
+      ) : null;
     }
 
-    return (
-      <section className={`skylight-wrapper ${this.props.className}`}>
-        {overlay}
+    const renderModal = () => {
+      if (isVisible) {
+        finalStyle = assign({}, dialogStyles, styles.animationOpen);
+      } else {
+        finalStyle = assign({}, dialogStyles, styles.animationBase);
+      }
+      return ( 
         <div className="skylight-dialog" style={finalStyle}>
-          <a 
-            role="button" 
+          <a
+            role="button"
             className="skylight-close-button"
             onClick={() => this.onCloseClicked()}
             style={closeButtonStyle}
@@ -87,11 +123,21 @@ export default class SkyLightStateless extends React.Component {
             {this.props.closeButton || '\u00D7'}
           </a>
           {title}
-            {this.props.children}
+          {this.props.children}
+        </div>
+      );
+    }
+
+    return (
+      <section
+        className={`skylight-wrapper ${this.props.className}`}
+        style={wrapperStyles}
+      >
+        <div style={modalWrapper}>
+          {renderModal()}
         </div>
       </section>
     );
-    
   }
 }
 
